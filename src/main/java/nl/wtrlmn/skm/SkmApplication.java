01e9de5ad76ce45fe556f7770278b1487fc10e0b
zetaenege <jonka@wtrlmn.com>
2025-06-03 00:46:01 +0200

    First commit

---
+++ src/main/java/nl/wtrlmn/skm/SkmApplication.java
@@ -0,0 +1,13 @@
+package nl.wtrlmn.skm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkmApplication.class, args);
+	}
+
+}
